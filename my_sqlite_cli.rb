1c2fa497cfb7b6b439a5252ee9821a8f893eac3c
liam gress <gress_l@qwasar.io>
2023-06-22 00:49:19 +0000

    my_sqlite

--- my_sqlite_cli.rb
+++ my_sqlite_cli.rb
@@ -1,8 +1,55 @@
+require 'readline'
 require_relative 'my_sqlite_request'
 
-# request = MySqliteRequest.new
+class MySqliteCLI
+  def initialize
+    @request = MySqliteRequest.new
+  end
 
-# request.from('nba_player_data.csv')
-#         .select(['name', 'position'])
-#         .where('name', 'Alaa Abdelnaby')
-#         .run
+  def run
+    puts "Welcome to MySqlite CLI!"
+
+    loop do
+      line = Readline.readline('> ', true)
+      break if line.nil? || line.strip.downcase == 'exit'
+
+      process_command(line)
+    end
+
+    puts "Goodbye!"
+  end
+
+  def process_command(line)
+    command, *args = line.split
+    case command
+    when 'from'
+      table_name = args[0]
+      @request.from(table_name)
+      puts "FROM: #{table_name}"
+    when 'select'
+      columns = args
+      @request.select(*columns)
+      puts "SELECT: #{columns.join(', ')}"
+    when 'where'
+      column_name, criteria = args
+      @request.where(column_name, criteria)
+      puts "WHERE: #{column_name} = '#{criteria}'"
+    when 'join'
+      column_a, filename_b, column_b = args
+      @request.join(column_a, filename_b, column_b)
+      puts "JOIN: #{column_a} = #{filename_b}.#{column_b}"
+    when 'order'
+      order, column_name = args
+      @request.order(order, column_name)
+      puts "ORDER: #{column_name} #{order}"
+    when 'run'
+      result = @request.run
+      puts "Query executed. Result: #{result}"
+    else
+      puts "Invalid command: #{command}"
+    end
+  end
+end
+
+cli = MySqliteCLI.new
+cli.run
--- my_sqlite_request.rb
+++ my_sqlite_request.rb
@@ -1,79 +1,54 @@
 require 'sqlite3'
 require 'csv'
 
-
 class MySqliteRequest
-    def initialize
+  def initialize
+    @table_name = nil
+    @select_columns = []
+    @where_conditions = []
+    @join_table = nil
+    @join_condition = nil
+    @order = nil
+  end
 
-        # file_path = 'C:\Users\lgres\Downloads\nba_player_data.csv'
-        # CSV.foreach(file_path) do |row|
-        #     puts row.inspect
-        # end
+  def from(table_name)
+    @table_name = table_name
+    self
+  end
 
-        #CSV.read("nba_player_data.csv")
-        @table_name = nil;
-        @select_columns = [];
-        @where_conditions = [];
-        @join_table = nil;
-        @join_condition = nil;
-        @query = {};
-        @query_result = nil;
+  def select(*column_names)
+    @select_columns = column_names.flatten
+    self
+  end
+
+  def where(*conditions)
+    @where_conditions = conditions.flatten
+    self
+  end
+
+  def join(column_a, table_b, column_b)
+    @join_table = table_b
+    @join_condition = "#{@table_name}.#{column_a} = #{@join_table}.#{column_b}"
+    self
+  end
+
+  def order(order, column_name)
+    @order = "#{column_name} #{order}"
+    self
+  end
+
+  def run
+    select_columns = @select_columns.join(', ')
+    query = "SELECT #{select_columns} FROM #{@table_name} #{generate_conditions};"
+    db = SQLite3::Database.new(@database_name)
+    db.execute(query) do |row|
+      puts row.join(', ')
     end
-
-    def from(table_name)
-        @table_name = CSV.read(table_name, headers: true);
-        self;
-    end
-
-    def select(*column_name)
-        @select_columns = column_name.flatten;
-        self;
-    end
-
-    def where(column_name, criteria)
-        condition = "#{column_name} = '#{criteria}'"
-        @query[:where] ||= [];
-        @query[:where].push(condition)
-        self;
-    end
-
-    def join(column_a, filename_b, column_b)
-        @join_table = filename_b;
-        @join_condition = "#{@table_name}.#{column_a} = #{@join_table}.#{column_b}";
-    end
-
-    def order(order, column_name)
-        @order = "#{column_name} #{order}";
-        self;
-    end
-
-    def insert(table_name)
-        @insert_table = table_name
-        self
-    end
-
-    def self.insert(table_name)
-        request = self.new
-        request.insert(table_name)
-        request
-      end    
-
-    def values(data)
-        @insert_values =  data;
-        self;
-    end
-
-    def update(table_name)
-        @update_table = table_name;
-        self;
-    end
-
-    def set(data)
-        @update_values = data;
-        self;
-    end
-
-    def run
-        
-    end
+  end  
 end
+
+MySqliteRequest.new.select('Player').from('test.csv').where("Player = 'Cliff Barker'").run
+
+
+
+
