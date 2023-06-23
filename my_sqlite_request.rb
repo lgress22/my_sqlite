b3be8136413fa7a5bede6e398953829c0fada2c9
liam gress <gress_l@qwasar.io>
2023-06-23 01:44:20 +0000

    mt_sqlite

--- my_sqlite_request.rb
+++ my_sqlite_request.rb
@@ -1,19 +1,37 @@
-require 'sqlite3'
 require 'csv'
 
 class MySqliteRequest
   def initialize
+    # db = SQLite3::Database.new("database.db")
     @table_name = nil
     @select_columns = []
     @where_conditions = []
     @join_table = nil
     @join_condition = nil
     @order = nil
+    @insert_data = nil
+    
   end
+
+#   def create_table
+#     db.execute("CREATE TABLE IF NOT EXISTS test (
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         Player TEXT,
+#         Height INT,
+#         Weight INT,
+#         Collage TEXT,
+#         Born INT,
+#         Birth_city TEXT,
+#         Birth_state TEXT)")
+#   end
 
   def from(table_name)
     @table_name = table_name
     self
+  end
+
+  def self.from(table_name)
+    new.from(table_name)
   end
 
   def select(*column_names)
@@ -37,18 +55,54 @@ class MySqliteRequest
     self
   end
 
-  def run
-    select_columns = @select_columns.join(', ')
-    query = "SELECT #{select_columns} FROM #{@table_name} #{generate_conditions};"
-    db = SQLite3::Database.new(@database_name)
-    db.execute(query) do |row|
-      puts row.join(', ')
+  def insert(table_name)
+    @table_name = table_name
+    self
+  end
+
+  def self.insert(table_name)
+    new.insert(table_name)
+  end
+
+  def values(data)
+    @insert_data = data
+    self
+  end
+
+#   def generate_conditions
+#     conditions = []
+#     conditions << "WHERE " + @where_conditions.join(' AND ') unless @where_conditions.empty?
+#     conditions << "JOIN #{@join_table} ON #{@join_condition}" if @join_table && @join_condition
+#     conditions.join(' ')
+#   end  
+
+   def run
+    result = []
+
+    CSV.foreach(@table_name, headers: true) do |row|
+        if @where_conditions.empty? || evaluate_conditions(row)
+          result_row = {}
+          @select_columns.each { |column| result_row[column] = row[column] }
+          result << result_row
+        end
     end
-  end  
+    result.each do |row|
+        puts row
+      end
+  
+    result
+    
+   end  
+
+   def evaluate_conditions(row)
+        @where_conditions.all? do |condition|
+            eval(condition, binding)
+        end
+    end
 end
 
-MySqliteRequest.new.select('Player').from('test.csv').where("Player = 'Cliff Barker'").run
+MySqliteRequest.new.select('Birth_city').from('test.csv').where("row['Player'] == 'Cliff Barker'").run
 
 
-
+MySqliteRequest.insert('database.csv').values('firstname' => "Thomas", 'lastname' => "Anderson", 'age' => 33, 'password' => 'matrix').run()
 
