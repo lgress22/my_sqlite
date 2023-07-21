f10ec697d9ac0a65c620f96d21fdfe1005487688
liam gress <gress_l@qwasar.io>
2023-07-21 18:29:33 +0000

    my_sqlite

--- my_sqlite_request.rb
+++ my_sqlite_request.rb
@@ -3,17 +3,10 @@ require 'csv'
 class MySqliteRequest
   def initialize
     @table_name = nil
-    @select_columns = []
-    @where_conditions = []
-    @join_table = nil
-    @join_condition = nil
-    @order = nil
-    @insert_data = []
-    @conditions = {}
-  end
-
-  def self.from(table_name)
-    new.from(table_name)
+    @columns = []
+    @conditions = []
+    @joins = []
+    @order_by = nil
   end
 
   def from(table_name)
@@ -21,121 +14,108 @@ class MySqliteRequest
     self
   end
 
-  def insert(table_name)
-    @table_name = table_name
+  def select(*columns)
+    @columns = columns.flatten
     self
   end
 
-  def self.insert(table_name)
-    new.insert(table_name)
-  end
-
-  def select(*column_names)
-    @select_columns = column_names.flatten
-    database_data = CSV.read(@table_name, headers: true)
-    @selected_data = database_data.map do |row|
-      selected_row = {}
-      column_names.each do |column_name|
-        selected_row[column_name] = row[column_name]
-      end
-      selected_row
-    end
-    self
-  end
-  
-
-  def where(column_name, criteria)
-    @where_conditions << { column_name: column_name, criteria: criteria }
+  def where(column, value)
+    @conditions << { column: column, value: value }
     self
   end
 
   def join(column_a, table_b, column_b)
-    @join_table = table_b
-    @join_condition = { column_a: column_a, column_b: column_b }
+    @joins << { column_a: column_a, table_b: table_b, column_b: column_b }
     self
   end
 
-  def order(order, column_name)
-    @order = "#{column_name} #{order}"
+  def order(column, direction = 'ASC')
+    @order_by = column
+    @order_direction = direction.upcase
     self
   end
 
-  def values(data)
-    @insert_data << data
-    self
-  end
 
   def run
-    result = []
-  
-    database_data = CSV.read('database.csv', headers: true)
-    test_data = CSV.read('test.csv', headers: true)
-  
-    database_columns = @select_columns.select { |column| column.start_with?('database.') }.map { |column| column.split('.')[1] }
-    test_columns = @select_columns.select { |column| column.start_with?('test.') }.map { |column| column.split('.')[1] }
-  
-    if @join_condition
-      join_rows = database_data.select do |database_row|
-        test_data.any? do |test_row|
-          database_row[@join_condition[:column_a]] == test_row[@join_condition[:column_b]]
-        end
-      end
-  
-      join_rows.each do |database_row|
-        test_rows = test_data.select { |test_row| database_row[@join_condition[:column_a]] == test_row[@join_condition[:column_b]] }
-        test_rows.each do |test_row|
-          result_row = {}
-          database_columns.each do |column|
-            result_row[column] = database_row[column]
-          end
-          test_columns.each do |column|
-            result_row[column] = test_row[column]
-          end
-          result << result_row
-        end
-      end
-    else
-      result_row = {}
-      database_columns.each do |column|
-        result_row[column] = nil
-      end
-      result << result_row
+    data = read_csv_file(@table_name)
+
+    @joins.each do |join|
+      column_a = join[:column_a]
+      table_b = join[:table_b]
+      column_b = join[:column_b]
+
+      join_data = read_csv_file(table_b)
+      data = apply_join(data, join_data, column_a, column_b)
     end
-  
-    if result.empty?
-        puts "No results found."
-      else
-        result.each do |row|
-          puts row unless row.empty?
-        end
-      end
-      
-  
-    append_data_to_csv if @insert_data.any?
-  
-    result
+
+    data = apply_conditions(data)
+    data = apply_column_selection(data)
+    data = apply_order(data)
+
+    data
   end
-  
 
   private
 
-  def append_data_to_csv
-    CSV.open(@table_name, 'a') do |csv|
-      csv << [] if File.zero?(@table_name) # Append an empty row if the file is empty
-      @insert_data.each do |data|
-        csv << data
+  def read_csv_file(file_name)
+    CSV.read(file_name, headers: true)
+  end
+
+  def apply_join(data, join_data, column_a, column_b)
+    join_data_hash = Hash[join_data.map { |row| [row[column_b.to_s], row] }]
+
+    data.map do |row|
+      join_value = row[column_a.to_s]
+      joined_row = row.to_h.merge(join_data_hash[join_value].to_h)
+      joined_row.delete(column_b.to_s) # Remove duplicate column
+      joined_row
+    end
+  end
+
+  def apply_conditions(data)
+    return data if @conditions.empty?
+
+    @conditions.each do |condition|
+      column = condition[:column]
+      value = condition[:value]
+
+      data = data.select { |row| row[column.to_s] == value }
+    end
+
+    data
+  end
+
+  def apply_column_selection(data)
+    if @columns.empty?
+      data
+    else
+      data.map do |row|
+        row.select { |column, _| @columns.include?(column) }
       end
     end
-  
-    puts 'Data inserted successfully.'
   end
-  
+
+  def apply_order(data)
+    return data unless @order_by
+
+    sorted_data = data.sort_by { |row| row[@order_by.to_s] }
+
+    sorted_data.reverse! if @order_direction == 'DESC'
+
+    sorted_data
+
+  end
 end
 
 request = MySqliteRequest.new
+request.from('test.csv')
+        .select('Player', 'firstname')
+        .join('ID', 'database.csv', 'ID')
+        .where('Player', 'Curly Armstrong')
+        .where('firstname', 'Thomas')
+        .order('Player', 'ASC')
+results = request.run
 
-request.select('firstname').from('database.csv')
-# request.insert('database.csv')
-# request.values(['2', 'John', 'Doe', '30', 'New York'])
-# request.values(['3', 'Jane', 'Smith', '25', 'Los Angeles'])
- request.run
+results.each do |row|
+  puts row.inspect
+end
