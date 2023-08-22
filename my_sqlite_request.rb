ff18c06b6fb3f24836c4616a0ad0b4a1ee78a41a
lgress22 <lgress10@gmail.com>
2023-08-22 16:56:34 -0700

    my_sqlite

--- my_sqlite_request.rb
+++ my_sqlite_request.rb
@@ -11,7 +11,10 @@ class MySqliteRequest
     @insert_table = nil
     @insert_data = []
     @insert_mode = false
+    @update_table = nil
+    @update_mode = false
     @update_data = {}
+    @file_name = nil
   end
 
   def from(table_name)
@@ -46,18 +49,14 @@ class MySqliteRequest
     self
   end
 
-  def values(column, value)
-    @update_data[column] = value
+  def values(data)
+    @insert_data << data
     self
   end
 
-  def update(file_path)
-    @file_path = file_path
-    self
-  end
-
-  def set(data)
-    @update_data = data
+  def update(table_name)
+    @update_table = table_name
+    @update_mode = true
     self
   end
 
@@ -66,9 +65,29 @@ class MySqliteRequest
     self
   end
 
-  def read_csv_file(file_name)
-    CSV.read(file_name, headers: true)
+  def read_csv_file(table_name)
+    @csv_files = ['test.csv', 'database.csv']
+  
+    csv_data_combined = [] # Combined data from all CSV files
+  
+    @csv_files.each do |csv_file|
+      csv_data = []
+  
+      CSV.foreach(csv_file, headers: true) do |row|
+        csv_data << row
+      end
+  
+      csv_data_combined += csv_data # Combine data from each CSV file
+    end
+  
+    puts "Is this working?"
+  
+    csv_data_combined.each do |row|
+      puts row.inspect
+    end
   end
+  
+
 
   def apply_join(data, join_data, column_a, column_b)
     join_data_hash = Hash[join_data.map { |row| [row[column_b.to_s], row] }]
@@ -88,13 +107,13 @@ class MySqliteRequest
       column = condition[:column]
       value = condition[:value]
 
-      data = data.select { |row| row[column.to_s] == value }
+      data = data.select { |row| row[column.to_i] == value }
     end
 
     data
   end
 
-  def apply_column_selection(data)
+  def apply_column(data)
     if @columns.empty?
       data
     else
@@ -114,98 +133,98 @@ class MySqliteRequest
     sorted_data
   end
 
-  def execute_insert
-    # Read the original data
-    data = read_csv_file(@insert_table)
-
-    # Append the new rows to the original data
-    @insert_data.each do |row|
-      data << CSV::Row.new(data.headers, row)
-    end
-
-    # Write the updated data back to the CSV file
-    CSV.open(@insert_table, 'wb') do |csv|
-      csv << data.headers
-      data.each do |row|
-        csv << row
-      end
-    end
-
-    @insert_data = [] # Reset insert_data after executing the insert
-  end
-
-  def apply_update
-    data = read_csv_file(@file_path)
-
-    data.each do |row|
-      if @where_condition.nil? || row[@where_condition[:column]] == @where_condition[:value]
-        @update_data.each do |column, value|
-          row[column] = value
+  def apply_insert
+    return if @csv_files.nil? || @csv_files.empty?
+  
+    @csv_files.each do |csv_file|
+      existing_data = read_csv_file(csv_file)
+      combined_data = existing_data + @insert_data
+  
+      CSV.open(csv_file, 'wb') do |csv|
+        csv << combined_data.first.headers
+        combined_data.uniq! # Remove duplicates
+        combined_data.each do |row|
+          csv << row
         end
       end
     end
+  
+    @insert_data = [] # Clear insert data after writing
+  end
+  
 
-    CSV.open(@file_path, 'w', write_headers: true, headers: data.first.headers) do |csv|
+  def apply_update(data)
+    return unless @update_mode
+  
+    data_by_col = data.by_col # Convert data into columns
+  
+    data.each_with_index do |row, index|
+      if row[@conditions[0][:column]] == @conditions[0][:value]
+        @update_data.each do |column, value|
+          data_by_col[column][index] = value
+        end
+      end
+    end
+  
+    CSV.open(@table_name, 'wb', headers: data.headers) do |csv|
       csv << data.headers
       data.each { |row| csv << row }
-    end
+    end  
   end
-
-
-
+  
+  
   def run
     data = read_csv_file(@table_name)
-
+  
     @joins.each do |join|
       column_a = join[:column_a]
       table_b = join[:table_b]
       column_b = join[:column_b]
-
+  
       join_data = read_csv_file(table_b)
       data = apply_join(data, join_data, column_a, column_b)
     end
-
+  
     data = apply_conditions(data)
-    data = apply_column_selection(data)
+    data = apply_column(data)
     data = apply_order(data)
-
-    if @insert_mode
-      execute_insert
-    elsif !@update_data.empty?
-      apply_update
-    end
-
-
+    apply_insert
+    data = apply_update(data)
+    
     data
   end
+  
 
   private
 end
   
+
   
 
 request = MySqliteRequest.new
 
 # request.from('test.csv')
 #        .select('Player', 'firstname')
-#        .join('ID', 'database.csv', 'ID')
-#        .where('Player', 'Curly Armstrong')
-#        .order('Player', 'DESC')
+#        #.join('ID', 'database.csv', 'ID')
+#        #.where('Player', 'Curly Armstrong')
+#        #.order('Player', 'DESC')
 #   results = request.run
 
+  
+
+ request.insert('database.csv')
+        .values(['4', 'Jill', 'Pilinsky', '35', 'El Dorado'])
+        results = request.run
+
+# request.update('database.csv')
+#     request.set('firstname', 'Jim')
+#     .set('lastname', 'Wilson')
+#     .where('ID', '2')
+#      results = request.run('database.csv')
 
 
-# request.insert('database.csv')
-#        .values(['3', 'John', 'Smith', '33', 'Wisconsin'])
-
-request.update('database.csv')
-    request.set('firstname', 'John')
-    .where('ID', '3')
-    results = request.run
 
 
-
-
-results.each do |row|
-    puts row.inspect
-  end
+     results.each do |row|
+      puts row.inspect
+    end
